85caf78aac4d4df893e7646f04f774f41a1e3b57
SunChanKwon <48266978+SunChanKwon@users.noreply.github.com>
2021-06-21 14:21:50 +0900

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,10 @@
+package sec06.exam01;
+
+public class Hello {
+	private static java.lang.Object System;
+
+	public static <String> void main(String[] args) {
+		
+		System.out.println("Hello, welcome to the java world!");
+	}
+}
