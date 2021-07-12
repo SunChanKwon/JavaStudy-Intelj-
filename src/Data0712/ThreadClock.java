aa321ca15e1e19d2113fa074de32394b7911e73b
D-Kwon <ksc7662@naver.com>
2021-07-12 09:57:55 +0900

    commit Clock

--- src/Data0712/ThreadClock.java
+++ src/Data0712/ThreadClock.java
@@ -1,37 +1,28 @@
 package Data0712;
 
-import java.awt.*;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.SimpleTimeZone;
-import java.util.TimeZone;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-public class ThreadClock extends JFrame{
-    JLabel Korean = new JLabel();
-    JLabel USA = new JLabel();
-
-
+public class ThreadClock extends JFrame implements Runnable{
+   Calendar now;
+   JLabel lbl = new JLabel("00:00:00",JLabel.CENTER);
+   int x,y;
     public ThreadClock(){
-        USA.setBounds(31,40,67,15);
-        add(USA);
-        Korean.setBounds(35,40,67,15);
-        add(Korean);
+    super("시계");
+    add(lbl);
+    }
 
+    public ThreadClock(int x, int y)
+    {
+        this();
+        this.x=x;
+        this.y=y;
+        setBounds(x,y,500,200);
         setSize(500,500);
         setVisible(true);
-        setTime();
-        while(true){
-            try{
-                setTime();
-                Thread.sleep(1000);
-            }catch(Exception e){}
-
-        }
-
     }
 
     public void setTime() {
@@ -46,17 +37,39 @@ public class ThreadClock extends JFrame{
         String timeStr2 = ("미국시간:"+ year + "년" + month + "월" + hour + "시간" + minute + "분" + second + "초");
 
         System.out.println("한국시간:"+ year + "년" + month + "월" + hour + "시간" + minute + "분" + second + "초");
-        System.out.println("미국시간:"+ year+1 + "년" + month + "월" + hour + "시간" + minute + "분" + second + "초");
+        System.out.println("미국시간:"+ year + "년" + month + "월" + hour + "시간" + minute + "분" + second + "초");
 
+    }
 
-        Korean.setText(timeStr1);
-        USA.setText(timeStr1);
+    @Override
+    public void run() {
+        do {
+            lbl.setText(getTimer());
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+            }
+        }while(true);
+    }
+    public  String getTimer(){
+        now =Calendar.getInstance();
+        SimpleDateFormat fmt = new SimpleDateFormat("HH:mm:ss");
+        return fmt.format(now.getTime());
     }
 
     public static void main(String[] args) {
-        new ThreadClock();
+//        ThreadClock dc1= new ThreadClock(0,0);
+//        Thread t1 = new Thread(dc1);
+//        t1.start();
+//
+//        ThreadClock dc2 = new ThreadClock(500,0);
+//        Thread t2 = new Thread(dc2);
+//        t2.start();
+//
+//        ThreadClock dc3 = new ThreadClock(500,0);
+//        Thread t3 = new Thread(dc3);
+//        t3.start();
     }
-
 }
 
 
